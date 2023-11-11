8f968733dfe225928bebed061801df41d78d1c25
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-11 16:03:57 +0100

    Add App component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Homepage from "../../pages/Homepage";
+
+const App = (): React.ReactElement => {
+  return (
+    <Routes>
+      <Route path="/home" element={<Homepage />} />
+      <Route path="/" element={<Navigate to="/home" />} />
+    </Routes>
+  );
+};
+
+export default App;
