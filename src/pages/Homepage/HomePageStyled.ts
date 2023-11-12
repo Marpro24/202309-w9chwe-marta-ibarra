93e927a6491340ac11e5d2755172ae9eb257cc49
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-12 13:05:01 +0100

    Add Homepage styles & route

---
+++ src/pages/Homepage/HomePageStyled.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const HomepageStyled = styled.main`
+  .title-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+    padding: 20px;
+    background-color: #e99441;
+    border-radius: 30px;
+    border: solid;
+    border-color: #fff;
+    position: fixed;
+    width: 100%;
+  }
+`;
+
+export default HomepageStyled;
