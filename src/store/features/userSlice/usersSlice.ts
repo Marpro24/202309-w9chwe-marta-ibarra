a2fb848e5c611e53ff7462d5a148661ad1a38867
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-12 17:17:52 +0100

    Add usersSlice & fix type

---
+++ src/store/features/userSlice/usersSlice.ts
@@ -0,0 +1,21 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { UsersStructure } from "../../types";
+import { User } from "../../types";
+
+const initialUsersState: UsersStructure = {
+  user: [],
+};
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState: initialUsersState,
+  reducers: {
+    loadUsers: (
+      currentState,
+      action: PayloadAction<User[]>,
+    ): UsersStructure => ({ ...currentState, user: action.payload }),
+  },
+});
+
+export const { loadUsers: loadUsersActionCreator } = usersSlice.actions;
+export const usersReducer = usersSlice.reducer;
