af366102bf6f7732f45795c5796ea63305e8dc1f
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-12 14:36:47 +0100

    Fix image size

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -8,13 +8,7 @@ const UserCard = ({ user }: UserProps): React.ReactElement => {
   return (
     <UserCardStyled className="user">
       <article className="user-container">
-        <img
-          src={user.image}
-          alt={user.name}
-          className="user__image"
-          width={400}
-          height={400}
-        />
+        <img src={user.image} alt={user.name} className="user__image" />
         <h2 className="user__name">{user.name}</h2>
         <span className="user__world">{`World: ${user.world}`}</span>
         <span>{`Friend or foe:${user.isFriend}`}</span>
