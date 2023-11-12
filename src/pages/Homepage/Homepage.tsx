ebd8e969b98621d77a4b01dbc9cc17eb62f8b8be
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-12 14:38:29 +0100

    Fix elements classes

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -2,13 +2,13 @@ import UsersList from "../../components/UsersList/UsersList";
 import HomepageStyled from "./HomePageStyled";
 const Homepage = (): React.ReactElement => {
   return (
-    <HomepageStyled>
-      <header className="title-container">
+    <>
+      <HomepageStyled className="title-container">
         <h1 className="title">Welcome to the Labyrinth</h1>
         <h2>Find your friends...and your foes</h2>
-      </header>
+      </HomepageStyled>
       <UsersList />
-    </HomepageStyled>
+    </>
   );
 };
 
