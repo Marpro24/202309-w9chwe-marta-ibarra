4dee64ff93828a569d58b56d7862fc1689d1abc6
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-12 01:36:47 +0100

    Add UserlsList test

---
+++ src/components/UsersList/UsersList.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import mainTheme from "../../styles/mainTheme";
+import UsersList from "./UsersList";
+
+describe("Given a Homepage ocmonent", () => {
+  describe("When it is renered and receives a list of people", () => {
+    test("Then it should show a users list with Ambrosius' name in the first heading", () => {
+      const expectedName = "Ambrosius";
+
+      render(
+        <BrowserRouter>
+          <ThemeProvider theme={mainTheme}>
+            <UsersList />
+          </ThemeProvider>
+        </BrowserRouter>,
+      );
+
+      const title = screen.getByRole("heading", { name: expectedName });
+
+      expect(title).toBeInTheDocument();
+    });
+  });
+});
