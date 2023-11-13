330f9c3a5e254db900fa434e6fb831d4918bfc95
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-13 08:27:37 +0100

    Refactor UsersList test

--- src/components/UsersList/UsersList.test.tsx
+++ src/components/UsersList/UsersList.test.tsx
@@ -1,22 +1,15 @@
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import mainTheme from "../../styles/mainTheme";
+import { customRenderWithProviders } from "../../testUtils/customRender";
+import { screen } from "@testing-library/react";
 import UsersList from "./UsersList";
+import mockUsers from "../../mocks/usersMocks";
 
 describe("Given a Homepage ocmonent", () => {
-  describe("When it is renered and receives a list of people", () => {
+  describe("When it is renered and receives the Goblin King data", () => {
     test("Then it should show a users list with Goblin King name in the first heading", () => {
       const expectedName = "Goblin King";
+      const mockList = mockUsers;
 
-      render(
-        <BrowserRouter>
-          <ThemeProvider theme={mainTheme}>
-            <UsersList />
-          </ThemeProvider>
-        </BrowserRouter>,
-      );
-
+      customRenderWithProviders(<UsersList />, mockList);
       const title = screen.getByRole("heading", { name: expectedName });
 
       expect(title).toBeInTheDocument();
